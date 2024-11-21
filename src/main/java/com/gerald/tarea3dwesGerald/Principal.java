8912f990fb06bd595189c905fe74f413e3b81b35
gerald <bdc65469@educastur.es>
2024-11-21 14:09:24 +0100

    Configurado arranque del proyecto

---
+++ src/main/java/com/gerald/tarea3dwesGerald/Principal.java
@@ -0,0 +1,17 @@
+package com.gerald.tarea3dwesGerald;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class Principal implements CommandLineRunner {
+	
+	//@Autowired
+
+	@Override
+	public void run(String... args) throws Exception {
+		//Este es el main
+		System.out.println("INI");
+		System.out.println("FIN");
+	}
+
+}
--- src/main/java/com/gerald/tarea3dwesGerald/Tarea3dwesGeraldApplication.java
+++ src/main/java/com/gerald/tarea3dwesGerald/Tarea3dwesGeraldApplication.java
@@ -2,9 +2,15 @@ package com.gerald.tarea3dwesGerald;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Tarea3dwesGeraldApplication {
+	
+	@Bean
+	public Principal applicationStartupRunner() {
+		return new Principal();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tarea3dwesGeraldApplication.class, args);
